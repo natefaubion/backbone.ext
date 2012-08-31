85140f5179764737f77ccc6e469300faecd4d8fa
Nathan Faubion <nathan@n-son.com>
2012-08-31 10:58:42 -0400

    Added a new example app.

---
+++ example/js/bootstrap.js
@@ -0,0 +1,14 @@
+App.contacts.reset([
+  { 
+    id: _.uniqueId(),
+    name: "Bill Branderly",
+    phone: "555-9876",
+    email: "bill@branderly.com"
+  },
+  {
+    id: _.uniqueId(),
+    name: "Greg Greggerson",
+    phone: "555-0468",
+    email: "greg@greggerson.com"
+  }
+]);
